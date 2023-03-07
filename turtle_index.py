bd7389ed10aa5ef50130c0290fc56b1bcc0c4ab9
Jordan Gent <talihashi@gmail.com>
2023-03-06 17:00:20 -0700

    Weird turtle has no dx attribute error

---
+++ turtle_index.py
@@ -0,0 +1,82 @@
+import turtle
+
+turtle.setup(400, 300)
+turtle.bgcolor("black")
+
+paddle1 = turtle.Turtle()
+paddle1.shape("square")
+paddle1.color("red")
+paddle1.shapesize(stretch_wid=5, stretch_len=1)
+paddle1.penup()
+paddle1.goto(-350, 0)
+paddle1.dy = 0
+
+paddle2 = turtle.Turtle()
+paddle2.shape("square")
+paddle2.color("red")
+paddle2.shapesize(stretch_wid=5, stretch_len=1)
+paddle2.penup()
+paddle2.goto(350, 0)
+paddle2.dy = 0
+
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("teal")
+ball.penup()
+ball.goto(0, 0)
+
+game_over = False
+winner = None
+points = {
+    "player1": 0,
+    "player2": 0
+}
+game_rules = {
+    "max_points": 3,
+    "ball_speed": 3
+}
+
+score_display = turtle.Turtle()
+score_display.color("white")
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, 260)
+score_display.write("Player 1: 0 Player 2: 0", align = "center", font = ("Arial", 24, "normal"))
+
+paddle1.sety(paddle1.ycor() + paddle1.dy)
+paddle2.sety(paddle2.ycor() + paddle2.dy)
+ball.setx(ball.xcor() + ball.dx)
+ball.sety(ball.ycor() + ball.dy)
+
+if points["player1"] == game_rules["max_points"]:
+    game_over = True
+    winner = "player1"
+elif points["player2"] == game_rules["max_points"]:
+    game_over = True
+    winner = "player2"
+
+if(ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle2.ycor() + 50 and ball.ycor() > paddle2.ycor() - 50):
+    ball.setx(340)
+    ball.dx *= -1
+elif (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle1.ycor() + 50 and ball.ycor() > paddle1.ycor() - 50):
+    ball.setx(-340)
+    ball.dx *= -1
+
+if ball.xcor() > 390:
+    ball.goto(0, 0)
+    ball.dx *= -1
+    points["player1"] += 1
+elif ball.xcor() < -390:
+    ball.goto(0, 0)
+    ball.dx *= -1
+    points["player2"] += 1
+
+if ball.ycor() > 290:
+    ball.sety(290)
+    ball.dy *= -1
+elif ball.ycor() < -290:
+    ball.sety(-290)
+    ball.dy *= -1
+
+score_display.clear()
+score_display.write(f"Player 1: {'player1'} Player 2: {'player2'}", align="center", font=("Arial", 24, "normal"))
